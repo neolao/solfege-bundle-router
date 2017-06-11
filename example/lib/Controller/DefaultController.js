c11c4d67b88851c3744b792caa59e698c470966d
neolao <contact@neolao.com>
2017-06-12 00:27:53 +0200

    Publish interface.js

--- example/lib/Controller/DefaultController.js
+++ example/lib/Controller/DefaultController.js
@@ -1,12 +1,18 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+    value: true
 });
 var DefaultController = class DefaultController {
-  *index(request, response) {
-    console.log("Homepage");
-  }
+    *index(request, response) {
+        console.log("Homepage");
+    }
+
+    *contact(request, response) {
+        console.log("Contact");
+
+        response.body = "Contact page";
+    }
 };
 exports.default = DefaultController;
 module.exports = exports["default"];
--- example/src/Controller/DefaultController.js
+++ example/src/Controller/DefaultController.js
@@ -13,4 +13,17 @@ export default class DefaultController
     {
         console.log("Homepage");
     }
+
+    /**
+     * Contact page
+     *
+     * @param   {Request}   request     HTTP request
+     * @param   {Response}  response    HTTP response
+     */
+    *contact(request, response)
+    {
+        console.log("Contact");
+
+        response.body = "Contact page";
+    }
 }
